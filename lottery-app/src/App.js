5399f7f5fcf92d0618997bf568ee3122e04d3a3d
Gabin <gabin.fay@gmail.com>
2024-10-19 12:24:59 -0700

    add: first working lottery

---
+++ lottery-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Lottery from './Lottery';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PolyLottery</h1>
+      </header>
+      <main>
+        <Lottery />
+      </main>
+    </div>
+  );
+}
+
+export default App;
